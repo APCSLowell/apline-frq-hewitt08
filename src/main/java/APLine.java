fe24b9268c663efaf28da3a1ef873e2407cb217b
hewitt08 <143777083+hewitt08@users.noreply.github.com>
2024-04-22 11:12:40 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,22 @@
 public class APLine
 {
-  /* your code here */
-  
+  private double slope;
+  private int a;
+  private int b;
+  private int c;
+  APLine(int A, int B, int C){
+      a = A;
+      b = B;
+      c = C;
+      slope = double(-a)/double(b);
+  }
+  public double getSlope(){
+      return slope;
+  }
+  public boolean isOnLine(int x, int y){
+      if((a*x)+(b*y)+c==0){
+        return true;
+      }
+    return false;
+  }
 }
